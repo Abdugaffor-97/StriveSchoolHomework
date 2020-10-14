716af55840bc5882e58cd6149985fc579a8467a1
Abdugaffor-97 <abdurahimov.97@list.ru>
2020-10-14 12:01:47 +0500

    added js file

---
+++ Module2/day2/index.js
@@ -0,0 +1,105 @@
+quranReciters = [
+    {
+        img: "images/mishari.jpeg",
+        name: "Mishari Rashid Al-Alafasi",
+        duration: "10 hours",
+        albumName: "Ramadan 2016",
+        about: "asdasd",
+    },
+    {
+        img: "images/mishari.jpeg",
+        name: "Mishari Rashid Al-Alafasi",
+        duration: "10 hours",
+        albumName: "Ramadan 2016",
+        about: "asdasd",
+    },
+    {
+        img: "images/mishari.jpeg",
+        name: "Mishari Rashid Al-Alafasi",
+        duration: "10 hours",
+        albumName: "Ramadan 2016",
+        about: "asdasd",
+    },
+    {
+        img: "images/mishari.jpeg",
+        name: "Mishari Rashid Al-Alafasi",
+        duration: "10 hours",
+        albumName: "Ramadan 2016",
+        about: "asdasd",
+    },
+    {
+        img: "images/mishari.jpeg",
+        name: "Mishari Rashid Al-Alafasi",
+        duration: "10 hours",
+        albumName: "Ramadan 2016",
+        about: "asdasd",
+    },
+    {
+        img: "images/mishari.jpeg",
+        name: "Mishari Rashid Al-Alafasi",
+        duration: "10 hours",
+        albumName: "Ramadan 2016",
+        about: "asdasd",
+    },
+    {
+        img: "images/mishari.jpeg",
+        name: "Mishari Rashid Al-Alafasi",
+        duration: "10 hours",
+        albumName: "Ramadan 2016",
+        about: "asdasd",
+    },
+    {
+        img: "images/mishari.jpeg",
+        name: "Mishari Rashid Al-Alafasi",
+        duration: "10 hours",
+        albumName: "Ramadan 2016",
+        about: "asdasd",
+    },
+    {
+        img: "images/mishari.jpeg",
+        name: "Mishari Rashid Al-Alafasi",
+        duration: "10 hours",
+        albumName: "Ramadan 2016",
+        about: "asdasd",
+    },
+    {
+        img: "images/mishari.jpeg",
+        name: "Mishari Rashid Al-Alafasi",
+        duration: "10 hours",
+        albumName: "Ramadan 2016",
+        about: "asdasd",
+    },
+    {
+        img: "images/mishari.jpeg",
+        name: "Mishari Rashid Al-Alafasi",
+        duration: "10 hours",
+        albumName: "Ramadan 2016",
+        about: "asdasd",
+    },
+    {
+        img: "images/mishari.jpeg",
+        name: "Mishari Rashid Al-Alafasi",
+        duration: "10 hours",
+        albumName: "Ramadan 2016",
+        about: "asdasd",
+    },
+];
+const reviewAlbum = document.querySelector("#review-album");
+
+const container = document.querySelector(".container");
+const row = container.querySelector(".row");
+document.addEventListener("DOMContentLoaded", () => {
+    const output = [];
+    quranReciters.forEach((reciter) => {
+        output.push(`
+        <div class="col col-6 col-md-4 col-lg-3">
+            <img src=${reciter.img} alt="${reciter.name}">
+            <a href="#"><h4>${reciter.albumName}</h4></a>
+            <p>${reciter.name}</p>
+            <small>Duration: ${reciter.duration}</small>
+        </div>
+        `);
+    });
+    row.innerHTML = output.join("");
+    console.log(reviewAlbum);
+});
