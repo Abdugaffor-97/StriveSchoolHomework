1393e5ec6c47e18135aa0eb1ecb6378f35331334
Abdugaffor-97 <abdurahimov.97@list.ru>
2020-10-15 19:57:07 +0500

    M2 day 4

---
+++ Module2/day4/script.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const generateBtn = document.getElementById("generate");
+
+    teamName = document.getElementById("team-name");
+    teamBtn = document.getElementById("team-add-btn");
+
+    playerName = document.getElementById("player-name");
+    playerBtn = document.getElementById("player-add-btn");
+
+    let countTeam = 0;
+    let teams = [];
+    teamBtn.addEventListener("click", function () {
+        teamsContainer = document.querySelector(".teams");
+        teamsContainer.innerHTML += `<div id="${countTeam}" class="team col col-6 col-lg-3">${teamName.value}</div>`;
+        countTeam += 1;
+        teamName.value = "";
+        teams.push(teamName.value);
+    });
+
+    playerBtn.addEventListener("click", function () {
+        listOfPlayes = document.querySelector("#list-of-players");
+        listOfPlayes.innerHTML += `<li>${playerName.value}</li>`;
+        playerName.value = "";
+    });
+
+    generateBtn.addEventListener("click", function () {
+        const shuffleArray = function (array) {
+            for (let i = array.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [array[i], array[j]] = [array[j], array[i]];
+            }
+            return array;
+        };
+
+        // console.log(players);
+
+        const teams = document.querySelectorAll(".team");
+        // console.log(teams);
+        const playersLi = document.querySelectorAll("li");
+        let players = [];
+        playersLi.forEach((player) => {
+            players.push(player.innerHTML);
+        });
+
+        players = shuffleArray(players);
+
+        let i = 0;
+        while (i !== players.length) {
+            console.log(players[i]);
+            teams.forEach((team) => {
+                team.appendChild(players[i])
+            });
+            i++;
+        }
+    });
+});
